44f7a238eb7edb47b5281f35c2693831bfb1b8cf
Matthew <mnj98@vt.edu>
2021-06-23 14:19:33 -0400

    Created signup and moved function

--- bot.js
+++ bot.js
@@ -4,9 +4,10 @@ const client = new Discord.Client();
 const {google} = require('googleapis');
 //const {GoogleAuth} = require('google-auth-library')
 const Sheets = require('./sheets.js')
+const signups = require('./signup.js')
 require('dotenv').config()
 
-const spreadsheetId = "1SA0twJDK9mkc-zwIaSLfDIEfssxd7dMszWRfYwMKnzY";
+const signup_spreadsheetId = "1SA0twJDK9mkc-zwIaSLfDIEfssxd7dMszWRfYwMKnzY";
 const apiKey = process.env.GOOGLE_API_KEY
 let auth
 let sheets
@@ -42,34 +43,7 @@ client.on('message', msg => {
         console.log(words)
 
         if(words[0] === '!signup'){
-            if(words.length <= 6 || words.length > 9) bad_signup(msg, 'syntax')
-            else {
-                let tier = words[2].toLowerCase()
-                if(tier === 'tier1' || tier === 'tier2' || tier === 'tier3'){
-                    words.push('')
-                    words.push('')
-                    sheets.spreadsheets.values.append({
-                        spreadsheetId: spreadsheetId,
-                        auth: auth,
-                        key: apiKey,
-                        range: tier + '!A:H',
-                        valueInputOption: 'USER_ENTERED',
-                        resource: {
-                            values: [['', words[1], words[3], words[4], words[5], words[6], words[7], words[8]]]
-                        }
-                    }, (err, res) =>{
-                        if(err){
-                            bad_signup(msg, 'backend error')
-                        }
-                        msg.reply('Signed up!')
-                    })
-
-                }
-                else bad_signup(msg, 'tier')
-
-
-
-            }
+            signups.signup_handler(words, msg, apiKey, auth, sheets, signup_spreadsheetId)
 
         }
 
@@ -77,9 +51,7 @@ client.on('message', msg => {
 
 });
 
-function bad_signup(msg, reason){
-    msg.reply('Incorrect ' + reason + ',\nuse: !signup \"team name\" TierX player1 player2 player3 player4')
-}
+
 
 
 Sheets.setup((sheet_auth) => {
---
+++ signup.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const {google} = require('googleapis');
+
+
+module.exports = {signup_handler}
+
+function signup_handler(words, msg, apiKey, auth, sheets, spreadsheetId){
+    if(words.length <= 6 || words.length > 9) bad_signup(msg, 'syntax')
+    else {
+        let tier = words[2].toLowerCase()
+        if(tier === 'tier1' || tier === 'tier2' || tier === 'tier3'){
+            words.push('')
+            words.push('')
+            sheets.spreadsheets.values.append({
+                spreadsheetId: spreadsheetId,
+                auth: auth,
+                key: apiKey,
+                range: tier + '!A:H',
+                valueInputOption: 'USER_ENTERED',
+                resource: {
+                    values: [['', words[1], words[3], words[4], words[5], words[6], words[7], words[8]]]
+                }
+            }, (err, res) =>{
+                if(err){
+                    bad_signup(msg, 'backend error')
+                }
+                msg.reply('Signed up!')
+            })
+
+        }
+        else bad_signup(msg, 'tier')
+
+
+
+    }
+}
+
+
+function bad_signup(msg, reason){
+    msg.reply('Incorrect ' + reason + ',\nuse: !signup \"team name\" TierX player1 player2 player3 player4')
+}
