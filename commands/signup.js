7d893a9a8c79d76fe01cbf70eab854825a0e3e0a
Matthew <mnj98@vt.edu>
2021-06-29 17:39:45 -0400

    Slash Commands

--- commands/signup.js
+++ commands/signup.js
@@ -6,7 +6,7 @@ module.exports = {
     description: 'Allows you to sign up for the tournament',
     minArgs: 6,
     maxArgs: 8,
-    expectedArgs: '<teamName> <tier> <player1> <player2> <player3> <player4> [sub1] [sub2]',
+    expectedArgs: '<team name> <tier> <player1> <player2> <player3> <player4> [sub1] [sub2]',
     callback: ({args}) => {
         console.log(args)
         return 'ok'
