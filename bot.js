1a357c7aca8aa9ebb1c1ae64d02ba6f042e4995f
Okri <matthew1156@gmail.com>
2021-07-01 01:02:38 -0400

    using CommandHandler as updated wok commands

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 global.client = new Discord.Client();
 const {google} = require('googleapis');
 const Sheets = require('./sheets.js')
-const WOKCommands = require('./WOKCommands')
+const WOKCommands = require('./CommandHandler')
 require('dotenv').config()
 
 //const signup_channel_id = '852287847696039969'
