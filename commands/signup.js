0188ade43cd08ccc7c64841ec7911f8e861db9a9
Matthew <mnj98@vt.edu>
2021-07-01 16:00:53 -0400

    Reorganized signup_handler to process errors in the correct order

--- commands/signup.js
+++ commands/signup.js
@@ -11,7 +11,7 @@ module.exports = {
     expectedArgs: '<team name> <tier> <player1> <player2> <player3> <player4> [sub1] [sub2]',
     argTypes: [3, 3, 9, 9, 9, 9, 9, 9],
     callback: async input => {
-        signup_handler.signup_handler(input.args, input.guild).then(response =>{
+        await signup_handler.signup_handler(input.args, input.guild).then(response =>{
             return response
         }).catch(err => {
             return 'Signup Failed: ' + err
