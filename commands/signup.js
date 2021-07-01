04a765792494f233eddb07b78967a49516bf424a
Matthew <mnj98@vt.edu>
2021-07-01 16:31:25 -0400

    Reorganized signup_handler to process errors in the correct order

--- commands/signup.js
+++ commands/signup.js
@@ -11,6 +11,7 @@ module.exports = {
     expectedArgs: '<team name> <tier> <player1> <player2> <player3> <player4> [sub1] [sub2]',
     argTypes: [3, 3, 9, 9, 9, 9, 9, 9],
     callback: async input => {
+        if(input.interaction.channel_id !== global.signup_channel_id) return 'Please use the signup channel'
         try{
             return await signup.signup_handler(input.args, input.guild)
         }
