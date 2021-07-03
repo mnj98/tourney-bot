4f117fd515fb055bfe1748351c06d218c01bf1a1
Matthew <mnj98@vt.edu>
2021-07-03 18:45:46 -0400

    cache users at startup

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-client = new Discord.Client();
 const {google} = require('googleapis');
 const sheets_setup = require('./sheets_setup.js')
 const WOKCommands = require('../CommandHandler')
@@ -9,8 +8,13 @@ process.env.signup_channel_id = '860309288529428480'
 process.env.server_id = '852287847696039966'
 process.env.signup_spreadsheetId = '1SA0twJDK9mkc-zwIaSLfDIEfssxd7dMszWRfYwMKnzY'
 
-client.on('ready', async () => {
+const intents = new Discord.Intents([
+    Discord.Intents.NON_PRIVILEGED, // include all non-privileged intents, would be better to specify which ones you actually need
+    "GUILD_MEMBERS", // lets you request guild members (i.e. fixes the issue)
+]);
+const client = new Discord.Client({ ws: { intents } });
 
+client.on('ready', async () => {
 
     const guild = await client.guilds.fetch(process.env.server_id)
     const members = await guild.members.fetch()
@@ -22,8 +26,6 @@ client.on('ready', async () => {
         testServers: [process.env.server_id],
         showWarns: false
     })
-
-
 })
 
 
@@ -31,7 +33,6 @@ sheets_setup.setup((sheet_auth) => {
     global.auth = sheet_auth
     global.sheets = google.sheets({version: 'v4', sheet_auth})
     client.login(process.env.BOT_TOKEN)
-
 })
 
 
