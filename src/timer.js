d9e8df0991a63f88db95447e8073b5cc3d544b72
Matthew <mnj98@vt.edu>
2021-07-04 18:29:42 -0400

    implemented timer

--- src/timer.js
+++ src/timer.js
@@ -6,6 +6,7 @@ module.exports = {start_timers, update_time}
 function start_timers(day, time){
     get_formatted_timer_data(day).then(teams => {
         teams.forEach(team => {
+            console.log(team[0])
             Stopwatch.get(team[0].toLowerCase(), {seconds: time}).on('end', () => time_up(team)).start()
         })
     }).catch(err => handle_err(err))
