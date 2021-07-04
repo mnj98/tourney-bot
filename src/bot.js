3984e0e80fa8bee97f89ba75caf3b37f22f19c06
Matthew <mnj98@vt.edu>
2021-07-04 15:46:00 -0400

    Implementing get timer data

--- src/bot.js
+++ src/bot.js
@@ -4,6 +4,8 @@ const sheets_setup = require('./sheets_setup.js')
 const WOKCommands = require('../CommandHandler')
 require('dotenv').config()
 
+const sh = require('./sheets.js')
+
 process.env.signup_channel_id = '860309288529428480'
 process.env.server_id = '852287847696039966'
 process.env.signup_spreadsheetId = '1SA0twJDK9mkc-zwIaSLfDIEfssxd7dMszWRfYwMKnzY'
@@ -18,6 +20,8 @@ client.on('ready', () => {
         testServers: [process.env.server_id],
         showWarns: false
     })
+
+    sh.get_timer_data().then(console.log).catch(console.log)
 })
 
 
--- src/sheets.js
+++ src/sheets.js
@@ -1,4 +1,4 @@
-module.exports = {append_line, get_num_signed_up, get_team_names}
+module.exports = {append_line, get_num_signed_up, get_team_names, get_timer_data}
 
 function get_num_signed_up(ids){
     return new Promise((resolve, reject) => {
@@ -73,6 +73,35 @@ function get_team_names(){
                     resolve(res.data.values[0][0].split('\n').map(teamName => teamName.toLowerCase()))
                 }
                 else resolve([])
+            }
+        })
+    })
+}
+
+function get_timer_data(){
+    return new Promise((resolve, reject) => {
+        global.sheets.spreadsheets.values.batchGet({
+            spreadsheetId: process.env.signup_spreadsheetId,
+            auth: global.auth,
+            key: process.env.GOOGLE_API_KEY,
+            ranges: [
+                'Tier1!B2:B1000',
+                'Tier1!I2:I1000',
+                'Tier1!K2:P1000',
+                'Tier2!B2:B1000',
+                'Tier2!I2:I1000',
+                'Tier2!K2:P1000',
+                'Tier3!B2:B1000',
+                'Tier3!I2:I1000',
+                'Tier3!K2:P1000'
+            ]
+        }, (err, res) => {
+            if(err){
+                console.log(err)
+                reject(err)
+            }
+            else{
+                resolve(res.data)
             }
         })
     })
