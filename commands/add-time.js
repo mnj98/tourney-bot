554eef5a59c607146b4e2a93754b179b1ce90e33
Matthew <mnj98@vt.edu>
2021-07-04 19:22:14 -0400

    slash command for add time

--- commands/add-time.js
+++ commands/add-time.js
@@ -3,14 +3,14 @@ const timer = require('../src/timer.js')
 module.exports = {
     slash: true,
     testOnly: true,
-    name: 'start-timers',
-    description: 'Starts tournament timers for a timeslot with a given time',
+    name: 'add-time',
+    description: 'Add time to a team\'s clock',
     minArgs: 2,
-    expectedArgs: '<time slot> <minutes>',
+    expectedArgs: '<team> <minutes>',
     argTypes: [3, 4],
     callback: (input) => {
         if(input.interaction.channel_id !== process.env.signup_channel_id) return 'Please use the signup channel'
-        timer.start_timers(input.args[0], input.args[1])
-        return 'Timers started'
+        timer.update_time(input.args[0], input.args[1])
+        return 'Time added'
     }
 }
