e8b0f6969c2d434ae5025fe7c9ffc7ab21f0b600
Matthew <mnj98@vt.edu>
2021-07-04 23:27:27 -0400

    permissions and time info

--- commands/timer-info.js
+++ commands/timer-info.js
@@ -13,19 +13,18 @@ module.exports = {
         if(!check_if.is_admin(input)) return 'You do not have permissions for this command'
 
         try{
-            const s = new Date()
             const info = await timer.get_timer_info(input.args[0])
-            console.log(new Date().valueOf() - s.valueOf())
+
             let response = 'Tournament Clock Information:\n'
 
             info.forEach(item => {
-                response += 'Team ' + item.name + ' has ' + (!item.has_completed ?
-                    (item.time_left + 'left') : 'run out of time') + '\n'
+                response += 'Team ' + item.name + ' has **' + (!item.has_completed ?
+                    (item.time_left + '**left') : '**run out of time') + '\n'
             })
             return response
         }catch(err){
-            console.log(err)
-            return 'Error :( ' + err
+            //console.log(err)
+            return 'Error :( ' //+ err
         }
 
     }
