2a936be321244044e0e2c4f01d6e3e83699f20ae
Matthew <mnj98@vt.edu>
2021-07-04 23:17:39 -0400

    permissions and time info

--- commands/timer-info.js
+++ commands/timer-info.js
@@ -11,6 +11,7 @@ module.exports = {
     argTypes: [3],
     callback: async (input) => {
         if(!check_if.is_admin(input)) return 'You do not have permissions for this command'
+        setTimeout(() => {return 'timeout, try again'}, 2800)
 
         try{
             const s = new Date()
